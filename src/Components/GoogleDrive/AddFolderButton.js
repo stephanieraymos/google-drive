9aa24b6a86871c59216f8ac2521b70802d964cce
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-16 11:30:50 -0700

    Font awesome icons

--- src/Components/GoogleDrive/AddFolderButton.js
+++ src/Components/GoogleDrive/AddFolderButton.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 
 const AddFolderButton = () => {
-    const openModal = () => {
-        
-    }
-  return <Button onClick={openModal} variant="outline-success" size="sm">Add</Button>;
+  const openModal = () => {};
+  return (
+    <Button onClick={openModal} variant="outline-success" size="sm">
+      <FontAwesomeIcon icon={faFolderPlus} />
+    </Button>
+  );
 };
 
 export default AddFolderButton;
