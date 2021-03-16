f8e464bcd70fe38cd86484987015bc4f389708cd
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-16 11:35:59 -0700

    Form Modal

--- src/Components/GoogleDrive/AddFolderButton.js
+++ src/Components/GoogleDrive/AddFolderButton.js
@@ -1,14 +1,43 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
 
 const AddFolderButton = () => {
-  const openModal = () => {};
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+
+
+  const openModal = () => {
+    setOpen(true);
+  };
+  const closeModal = () => {
+    setOpen(false);
+  };
+
+
   return (
+      <>
     <Button onClick={openModal} variant="outline-success" size="sm">
       <FontAwesomeIcon icon={faFolderPlus} />
     </Button>
+    <Modal show={open} onHide={closeModal}>
+        <Form>
+            <Modal.Body>
+                <Form.Group>
+                    <Form.Label>Folder Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        required
+                        value={name}
+                        onChange={e => SVGAnimateTransformElement(e.target.value)}
+                    />
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+        </Form>
+    </Modal>
+    </>
   );
 };
 
