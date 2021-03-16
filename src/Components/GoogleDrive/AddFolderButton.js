ad57740798ce738b71e8bf36b89dbaf123f17494
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-16 11:42:23 -0700

    Accessing files and folders from database + handleSubmit

--- src/Components/GoogleDrive/AddFolderButton.js
+++ src/Components/GoogleDrive/AddFolderButton.js
@@ -7,7 +7,6 @@ const AddFolderButton = () => {
   const [open, setOpen] = useState(false);
   const [name, setName] = useState("");
 
-
   const openModal = () => {
     setOpen(true);
   };
@@ -15,31 +14,41 @@ const AddFolderButton = () => {
     setOpen(false);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setName("");
+    closeModal();
+  };
 
   return (
-      <>
-    <Button onClick={openModal} variant="outline-success" size="sm">
-      <FontAwesomeIcon icon={faFolderPlus} />
-    </Button>
-    <Modal show={open} onHide={closeModal}>
-        <Form>
-            <Modal.Body>
-                <Form.Group>
-                    <Form.Label>Folder Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        required
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                    />
-                </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={closeModal}>Close</Button>
-                <Button variant="success" type="submit">Add Folder</Button>
-            </Modal.Footer>
+    <>
+      <Button onClick={openModal} variant="outline-success" size="sm">
+        <FontAwesomeIcon icon={faFolderPlus} />
+      </Button>
+      <Modal show={open} onHide={closeModal}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Folder Name</Form.Label>
+              <Form.Control
+                type="text"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+            </Button>
+            <Button variant="success" type="submit">
+              Add Folder
+            </Button>
+          </Modal.Footer>
         </Form>
-    </Modal>
+      </Modal>
     </>
   );
 };
