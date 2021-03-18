422b80e1c28f4a21bb0c548c565f2d8e2caffb79
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-18 15:28:15 -0700

    Files uploading to database

--- src/Components/GoogleDrive/AddFileButton.js
+++ src/Components/GoogleDrive/AddFileButton.js
@@ -2,7 +2,8 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileUpload } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../../Context/AuthContext";
-import { storage } from "../../firebase";
+import { storage, database } from "../../firebase";
+import { ROOT_FOLDER } from "../../Hooks/useFolder";
 
 const AddFileButton = ({ currentFolder }) => {
   const { currentUser } = useAuth();
@@ -12,16 +13,31 @@ const AddFileButton = ({ currentFolder }) => {
     if (currentFolder == null || file == null) return;
 
     const filePath =
-      currentFolder.path.length > 0
-        ? //If there's a path: put it at beginning of file
-          `${currentFolder.path.join("/")}/${file.name}`
-        : //Otherwise just put file name as path
-          file.name;
+      currentFolder === ROOT_FOLDER //If in root folder
+        ? `${currentFolder.path.join("/")}/${file.name}` //set path to current folder / name of file (ignore the folders name)
+        : `${currentFolder.path.join("/")}/${currentFolder.name}/${file.name}`; //Otherwise set path to current folder / current folder name / name of file
 
     //Saving upload task to: Root folder called files, new folder for individual user + file path
     const uploadTask = storage
       .ref(`/files/${currentUser.uid}/${filePath}`)
-      .put(file); //Put file inside of this location
+      .put(file) //Put file inside of this location
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      () => {},
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+          database.files.add({
+            url: url,
+            name: file.name,
+            createdAt: database.getCurrentTimeStamp(),
+            folderId: currentFolder.id,
+            userId: currentUser.uid,
+          });
+        });
+      }
+    );
   };
 
   return (
