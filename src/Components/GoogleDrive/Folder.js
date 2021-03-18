345cd6fd1490d36e308846d0013e7463ae7b9e88
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-18 13:28:20 -0700

    Made link a bootstrap button and set it to link

--- src/Components/GoogleDrive/Folder.js
+++ src/Components/GoogleDrive/Folder.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolder } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
 
 const Folder = ({ folder }) => {
   return (
-    <Link>
+    <Button as={Link}>
       <FontAwesomeIcon icon={faFolder} className="mr-2" />
       {folder.name}
-    </Link>
+    </Button>
   );
 };
 
