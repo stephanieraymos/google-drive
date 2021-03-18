16fe60b86b65ad1cd4ee13a5a7802787ca7ee115
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-18 13:37:42 -0700

    Routes working as expected

--- src/Components/GoogleDrive/Dashboard.js
+++ src/Components/GoogleDrive/Dashboard.js
@@ -4,10 +4,11 @@ import Navigation from "./Navbar";
 import AddFolderButton from "./AddFolderButton";
 import { useFolder } from "../../Hooks/useFolder";
 import Folder from "./Folder";
+import { useParams } from "react-router-dom";
 
 const Dashboard = () => {
-  const { folder, childFolders } = useFolder("Ak7G16kr7kLO7L3HA27r");
-  console.log(childFolders);
+  const { folderId } = useParams();
+  const { folder, childFolders } = useFolder(folderId);
 
   return (
     <>
@@ -16,7 +17,7 @@ const Dashboard = () => {
         <AddFolderButton currentFolder={folder} />
         {childFolders.length > 0 && (
           <div className="d-flex flex-wrap">
-            {childFolders.map(childFolder => (
+            {childFolders.map((childFolder) => (
               <div
                 key={childFolder.id}
                 style={{ maxWidth: "250px" }}
