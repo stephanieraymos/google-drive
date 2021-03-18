c6fb6f1bc507c839ecec7f5686c16680a622e20e
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-18 13:27:20 -0700

    Clickable folder with icon

--- src/Components/GoogleDrive/Folder.js
+++ src/Components/GoogleDrive/Folder.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
 
 const Folder = ({ folder }) => {
-  return <div>{folder.name}</div>;
+  return (
+    <Link>
+      <FontAwesomeIcon icon={faFolder} className="mr-2" />
+      {folder.name}
+    </Link>
+  );
 };
 
 export default Folder;
